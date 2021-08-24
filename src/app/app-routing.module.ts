6620b95806d1cf449f0aee7048eb15001a949554
Romel Palomino <romel.palomino@rappi.com>
2021-08-23 19:25:28 -0500

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component"
+import { ProductComponent } from "./components/product.component"
+import { ProductsComponent } from "./products/products.component"
+import { ContactComponent } from "./contact/contact.component"
+import { DemoComponent } from "./demo/demo.component"
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component"
+import { ProductDetailComponent } from "./product-detail/product-detail.component"
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: '/home',
+    pathMatch: "full"
+  },
+  {
+  path: "home",
+  component: HomeComponent
+  },{
+    path: "products",
+    component: ProductsComponent
+  },{
+    path: "contact",
+    component: ContactComponent
+  },{
+    path: "demo",
+    component: DemoComponent
+  },{
+    path: "products/:id",
+    component: ProductDetailComponent
+  },{
+    path: "**",
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
