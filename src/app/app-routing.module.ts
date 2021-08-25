ac92ad8e3a5efd9536ba8bab84e216d37493caae
Romel Palomino <romel.palomino@rappi.com>
2021-08-24 20:20:51 -0500

    Create module layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component"
-import { ProductComponent } from "./components/product.component"
+import { LayoutComponent } from "./layout/layout.component"
 import { ProductsComponent } from "./products/products.component"
 import { ContactComponent } from "./contact/contact.component"
 import { DemoComponent } from "./demo/demo.component"
@@ -12,24 +12,30 @@ import { ProductDetailComponent } from "./product-detail/product-detail.componen
 const routes: Routes = [
   {
     path: "",
-    redirectTo: '/home',
-    pathMatch: "full"
-  },
-  {
-  path: "home",
-  component: HomeComponent
-  },{
-    path: "products",
-    component: ProductsComponent
-  },{
-    path: "contact",
-    component: ContactComponent
+    component: LayoutComponent,
+    children:[
+      {
+        path: "",
+        redirectTo: '/home',
+        pathMatch: "full",
+      },
+      {
+        path: "home",
+        component: HomeComponent
+      },{
+        path: "products",
+        component: ProductsComponent
+      },{
+        path: "contact",
+        component: ContactComponent
+      },{
+        path: "products/:id",
+        component: ProductDetailComponent
+      }
+    ]
   },{
     path: "demo",
     component: DemoComponent
-  },{
-    path: "products/:id",
-    component: ProductDetailComponent
   },{
     path: "**",
     component: PageNotFoundComponent
